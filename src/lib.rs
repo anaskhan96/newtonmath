dba6ca289390e721cca72766336b242cd9807039
Anas Khan <anas.khan96@outlook.com>
2017-12-28 23:31:16 +0530

    simplify function added

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate reqwest;
+extern crate serde_json;
+
+use self::serde_json::Value;
+
+const ENDPOINT: &str = "https://newton.now.sh";
+pub type DataResult = Result<String, reqwest::Error>;
+
+pub fn simplify(expression: &str) -> DataResult {
+    let url = format!("{}/simplify/{}", ENDPOINT, expression);
+    let mut response = reqwest::get(&url)?;
+    let data: Value = serde_json::from_str(&response.text().unwrap()).unwrap();
+    Ok(String::from(format!("{}", data["result"])))
 }
