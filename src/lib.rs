3e2706b3f9cd938cd406854125432f86ee1f3ab9
Anas Khan <anas.khan96@outlook.com>
2017-12-28 23:45:11 +0530

    adding the other api functions

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,76 @@ pub fn simplify(expression: &str) -> DataResult {
     fetch_result(&url)
 }
 
+pub fn factor(expression: &str) -> DataResult {
+    let url = format!("{}/factor/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn derive(expression: &str) -> DataResult {
+    let url = format!("{}/derive/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn integrate(expression: &str) -> DataResult {
+    let url = format!("{}/integrate/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn find_zeroes(expression: &str) -> DataResult {
+    let url = format!("{}/zeroes/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn find_tangent(expression: &str) -> DataResult {
+    let url = format!("{}/tangent/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn area_under_curve(expression: &str) -> DataResult {
+    let url = format!("{}/area/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn cosine(expression: &str) -> DataResult {
+    let url = format!("{}/cos/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn sine(expression: &str) -> DataResult {
+    let url = format!("{}/sin/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn tangent(expression: &str) -> DataResult {
+    let url = format!("{}/tan/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn inverse_cosine(expression: &str) -> DataResult {
+    let url = format!("{}/arccos/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn inverse_sine(expression: &str) -> DataResult {
+    let url = format!("{}/arcsin/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn inverse_tangent(expression: &str) -> DataResult {
+    let url = format!("{}/arctan/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn absolute_value(expression: &str) -> DataResult {
+    let url = format!("{}/abs/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
+pub fn logarithm(expression: &str) -> DataResult {
+    let url = format!("{}/log/{}", ENDPOINT, expression);
+    fetch_result(&url)
+}
+
 fn fetch_result(url: &str) -> DataResult {
     let mut response = reqwest::get(url)?;
     let data: Value = serde_json::from_str(&response.text().unwrap()).unwrap();
